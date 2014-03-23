1a6dad156eb9184f99e2be73c98094d7bd9c7fd2
TomKnig <hi@tomknig.de>
2014-03-23 09:14:06 +0100

    invoke callback to determine when rendering did finish

--- once.js
+++ once.js
@@ -2,44 +2,61 @@ function Once(scriptId, args) {
     'use strict';
     
     var script = document.getElementById(scriptId),
+        numberOfRenderingObjects = 0,
+        isRenderingsAreSetUp = false,
         idx = 0;
     
-    this.renderingDidFinish = function (concrete) {
+    this.checkState = function () {
+        if (isRenderingsAreSetUp && numberOfRenderingObjects === 0) {
+            window.isOnceFinished = true;
+        }
+    };
+    
+    this.renderingDidFinish = function (object) {
         var initer = '',
             arg,
             id,
             i;
         
-        if (concrete.hasOwnProperty('args') && concrete.hasOwnProperty('init')) {
-            for (i = 0; i < concrete.args.length; i += 1) {
+        if (object.hasOwnProperty('args') && object.hasOwnProperty('init')) {
+            for (i = 0; i < object.args.length; i += 1) {
                 if (initer !== '') {
                     initer += ',';
                 }
-                arg = concrete.args[i];
+                arg = object.args[i];
                 initer += JSON.stringify(arg);
             }
         }
         
-        id = concrete.constructor.name + idx;
+        id = object.constructor.name + idx;
         idx += 1;
         
-        script.innerHTML += 'var ' + id + '=new ' + concrete.constructor.name + '(' + initer + ');';
+        script.innerHTML += 'var ' + id + '=new ' + object.constructor.name + '(' + initer + ');';
         script.innerHTML += id + '.init();';
+        
+        numberOfRenderingObjects -= 1;
+        this.checkState();
     };
     
     this.init = function () {
-        var abstract,
+        var object,
             i;
+        
+        window.isOnceFinished = false;
         
         script.innerHTML = '';
         
         for (i = 0; i < args.length; i += 1) {
-            abstract = args[i];
-            if (abstract.hasOwnProperty('render')) {
-                abstract.isRendered = this.renderingDidFinish;
-                abstract.render();
+            object = args[i];
+            if (object.hasOwnProperty('render')) {
+                numberOfRenderingObjects += 1;
+                object.isRendered = this.renderingDidFinish;
+                object.render();
             }
         }
+        
+        isRenderingsAreSetUp = true;
+        this.checkState();
     };
     
     window.onload = this.init();
